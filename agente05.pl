09c749f6b40952abad797031c562a98bc3e76eee
Joao Gabriel Gouveia De Souza Brito <gabriel.gouveia@live.com>
2015-12-03 12:44:28 -0300

    Correção de bug e implementação de casas por onde o agente passou

--- agente05.pl
+++ agente05.pl
@@ -32,12 +32,13 @@
 % ?- start.
 :-dynamic([
 agent_angulo/1,
-antiga_posicao/2,
-atual_posicao/2,
+antiga_posicao/1,
+atual_posicao/1,
 casas_seguras/1,
 agent_flecha/1,
 agent_ouro/1,
-wumpus/1
+wumpus/1,
+hole/1
 ]).
 
 :- load_files([wumpus3]).
@@ -46,8 +47,8 @@ wumpusworld(pit3, 4). %definindo 3 buracos fixos
 init_agent :- % se nao tiver nada para fazer aqui, simplesmente termine com um ponto (.)
     writeln('Agente iniciando...'), % apague esse writeln e coloque aqui as acoes para iniciar o agente
     retractall(agent_angulo(_)),
-    retractall(antiga_posicao([_,_])),
-    retractall(atual_posicao([_,_])),
+    retractall(antiga_posicao(_)),
+    retractall(atual_posicao(_)),
     retractall(casas_seguras(_)),
     retractall(agent_flecha(_)),
     retractall(agent_ouro(_)),
@@ -55,9 +56,9 @@ init_agent :- % se nao tiver nada para fazer aqui, simplesmente termine com um p
     assert(wumpus(1)),
     assert(agent_ouro(0)),
     assert(agent_flecha(1)),
-    assert(antiga_posicao([1,1])),
+    %assert(antiga_posicao([1,1])),
     assert(atual_posicao([1,1])),
-    assert(casas_seguras(casa(1,1))),
+    assert(casas_seguras(_)),
     assert(agent_angulo(0)).
     
 
@@ -85,11 +86,10 @@ writeln(Percepcao), % apague para limpar a saida. Coloque aqui seu codigo.
   imprima_antpos,
   imprima_flecha,
   imprima_ouro,
+  imprima_casas,
   imprima_wumpus.
   %forca(Percepcao, Acao).
   %inicio da inteligencia/reacoes do agente...
-
-
 
 doido([no,_,no,no,no],goforward):-
       atual_posicao([X,Y]),
@@ -98,7 +98,8 @@ doido([no,_,no,no,no],goforward):-
       retractall(atual_posicao([_,_])),
       retractall(antiga_posicao([_,_])),
       assert(antiga_posicao([X,Y])),
-      assert(atual_posicao([K,W])).
+      assert(atual_posicao([K,W])),
+      salva_pos_segura.
 
 doido([_,_,no,yes,_], turnleft):- 
       atual_posicao([X,Y]),
@@ -133,11 +134,12 @@ doido([yes,_,_,_,_],goforward):-atual_posicao([X,Y]),
 
 
 imprima_orien:-agent_angulo(X), writeln(X).  
-imprima_pos:- atual_posicao([X,Y]),write(X),write(','),writeln(Y).
-imprima_antpos:- antiga_posicao([X,Y]),write(X),write(','),writeln(Y).
+imprima_pos:- atual_posicao(LIST),writeln(LIST).
+imprima_antpos:- antiga_posicao(LIST),writeln(LIST).
 imprima_flecha:- agent_flecha(X), write('Flechas do agente: '), writeln(X).
 imprima_ouro:- agent_ouro(X), ((X=:=0, writeln('Estou sem Ouro')) | (X=:=1, writeln('Estou com o ouro'))).
 imprima_wumpus:- wumpus(X), ((X=:=0, writeln('Wumpus esta morto')) | (X=:=1, writeln('Wumpus esta vivo'))).
+imprima_casas:-casas_seguras(List), writeln(List).
 
 
 nova_posicao(X,Y,0,X1,Y):- X1 is (X+1).%o agent só andou pra frente
@@ -145,9 +147,14 @@ nova_posicao(X,Y,90,X,Y1):- Y1 is (Y+1). %o agent só andou pra cima
 nova_posicao(X,Y,180,X1,Y):- X1 is (X-1).%o agent só andou para trás.
 nova_posicao(X,Y,270,X,Y1):- Y1 is (Y-1). %o agent só andou para baixo.
 
-salva_pos_segura:-antiga_posicao([X,Y]),assert(casas_seguras(casa(X,Y))).
+salva_pos_segura:-antiga_posicao(LIST1), 
+            casas_seguras(List),
+            append(List,[LIST1] ,NewList),
+            retractall(casas_seguras(_)),
+            assert(casas_seguras(NewList)).
 dec_flecha:-agent_flecha(X), X1 is (X-1), retractall(agent_flecha(_)), assert(agent_flecha(X1)).
 set_ouro:-agent_ouro(X), X1 is (X+1), retractall(agent_ouro(_)), assert(agent_ouro(X1)).
+
 
 
 
